53df780bb90df8f23f03fbc11e1366bb84d4a184
LoafPickleWW <loafpickle@protonmail.com>
2024-07-22 12:49:12 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,11 @@ module.exports = {
       colors: {
         "primary-black": "#010002",
         "primary-gray": "#D0D0D1",
-        "primary-green": "#439E43",
+        "primary-green": "#f57b14",
         "secondary-gray": "#393C3A",
         "secondary-black": "#000a02",
         "asset-detail-bg": "#1e1e1e",
-        "secondary-green": "#6EFFBB",
+        "secondary-green": "#f57b14",
       },
       fontSize: {
         xxs: "0.6rem",
